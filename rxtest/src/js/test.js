bf0ac4647fe2896980bff3f026d80df132ddb6e2
catyee <15117992385@163.com>
2018-05-24 17:33:33 +0800

    更新题目

--- rxtest/src/js/test.js
+++ rxtest/src/js/test.js
@@ -1,4 +1,5 @@
 // import { esTest } from './common/es-test';
 // var esTest = require('./common/es-test');
 // var ajaxTest = require('./common/ajax.test');
-var arrayTest = require('./common/array.test');
+// var arrayTest = require('./common/array.test');
+require('./common/rm');
