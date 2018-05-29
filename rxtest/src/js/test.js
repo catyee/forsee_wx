3f53267108454d4b5eb0f88d3deb1918e1bdf3b8
catyee <15117992385@163.com>
2018-05-29 17:14:40 +0800

    Vue start

--- rxtest/src/js/test.js
+++ rxtest/src/js/test.js
@@ -3,3 +3,17 @@
 // var ajaxTest = require('./common/ajax.test');
 // var arrayTest = require('./common/array.test');
 require('./common/rm');
+console.log(1);
+console.log(2);
+setTimeout(function () {
+  console.log('setTimeout1');
+ 
+});
+
+setTimeout(function () {
+  console.log('setTimeout2');
+});
+Promise.resolve().then(function () {
+    console.log('promise');
+  });
+
